dd27b61863348caf971c00971cf45e9a8e8592b0
Roberto Hernández Robles <robertohr18@gmail.com>
2018-08-24 00:14:36 -0600

    Added landing page style

--- PunctualArt/PunctualArt/Controllers/LandingController.cs
+++ PunctualArt/PunctualArt/Controllers/LandingController.cs
@@ -4,9 +4,6 @@ namespace PunctualArt.Controllers
 {
     public class LandingController : Controller
     {
-        public IActionResult Landing()
-        {
-            return View("Views/Landing.cshtml");
-        }
+        public IActionResult Landing() => View("Views/Landing.cshtml");
     }
 }
