27835252e070ec5c9b572fb8181be48d8ddaef5c
Roberto Hernández Robles <robertohr18@gmail.com>
2018-08-23 22:22:11 -0600

    Added the Landing page

---
+++ PunctualArt/PunctualArt/Controllers/LandingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PunctualArt.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult Landing()
+        {
+            return View("Views/Landing.cshtml");
+        }
+    }
+}
--- PunctualArt/PunctualArt/Startup.cs
+++ PunctualArt/PunctualArt/Startup.cs
@@ -43,7 +43,7 @@ namespace PunctualArt
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Landing}/{action=Landing}/{id?}");
             });
         }
     }
